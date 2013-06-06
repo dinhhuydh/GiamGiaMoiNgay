798bd1cc5c8bafa93fbba333633c2146730d2a6c
Huy Dinh <dinhhuydh@gmail.com>
2013-06-06 19:52:43 +0700

    add validation to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :bought, :name, :price, :sale_by_day, :public_date,
                   :initial_price
+
+  validates_presence_of :name, :price, :sale_by_day, :public_date,
+                        :initial_price
 end
