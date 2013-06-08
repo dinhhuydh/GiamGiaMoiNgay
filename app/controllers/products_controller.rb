afc57886a2b153eb430cb9752a3dc208acf4b6b1
Huy Dinh <dinhhuydh@gmail.com>
2013-06-08 21:45:33 +0700

    basic action for Product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,30 @@ class ProductsController < ApplicationController
 
   def show
   end
+
+  def new
+    3.times { product.images.build }
+  end
+
+  def create
+    if product.save
+      redirect_to product_path(product)
+    else
+      flash[:error] = 'Something wrong'
+      render :new
+    end
+  end
+
+  def edit
+    product.images.build
+  end
+
+  def update
+    if product.save
+      redirect_to product_path(product)
+    else
+      flash[:error] = 'Something wrong'
+      render :new
+    end
+  end
 end
