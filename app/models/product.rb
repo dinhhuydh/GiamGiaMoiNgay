e640fb034e262caecb0b85d9f6a7ceb58f30b728
Huy Dinh <dinhhuydh@gmail.com>
2013-06-08 21:44:11 +0700

    product has many images

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,13 @@ class Product < ActiveRecord::Base
 
   attr_accessible :bought, :name, :price, :sale_by_day, :public_date,
                   :initial_price, :public_time, :slug,
+                  :images, :images_attributes
 
   validates_presence_of :name, :price, :sale_by_day, :public_date,
                         :initial_price, :public_time
+
+  has_many :images, :as => :owner, :dependent => :destroy
+  accepts_nested_attributes_for :images
 
   def next_sale_off_time
     next_time = public_time + eval("#{(Time.now.to_date - public_time.to_date).to_i}.day")
