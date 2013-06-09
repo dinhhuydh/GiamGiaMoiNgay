27c98482c48248dac9da25c247ce4486b457427a
Huy Dinh <dinhhuydh@gmail.com>
2013-06-09 16:42:15 +0700

    restyle show page for Product
    - display images
    - divide layout

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def product_image(product)
+    if product.images.present?
+      image_tag product.images.first.photo.url
+    else
+      image_tag image_path('default_product_image.jpg')
+    end
+  end
 end
