19994668334084f0a4f0a91522bf537724b12ceb
Huy Dinh <dinhhuydh@gmail.com>
2013-06-09 18:39:13 +0700

    add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130608102708) do
+ActiveRecord::Schema.define(:version => 20130609113647) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -68,6 +68,11 @@ ActiveRecord::Schema.define(:version => 20130608102708) do
     t.float    "initial_price"
     t.datetime "public_time"
     t.string   "slug"
+  end
+
+  create_table "users", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
