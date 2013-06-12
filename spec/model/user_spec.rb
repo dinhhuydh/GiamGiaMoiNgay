e63a538a6f7960e567beeb58f52a2c783d9fd9ef
Huy Dinh <dinhhuydh@gmail.com>
2013-06-12 22:14:50 +0700

    specs for User model

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -2,10 +2,19 @@ require 'spec_helper'
 
 describe User do
   let(:user) { create(:user) }
+  let(:product) { create(:product) }
 
   it { should have_many(:products_followers) }
+  it { should have_many(:followed_products) }
+  it { should have_many(:products_consumers) }
+  it { should have_many(:bought_products) }
 
   describe '#follow' do
-    pending
+    it 'should create a new relationship' do
+      expect{
+        user.follow(product)
+      }.to change(ProductsFollower, :count).by(1)
+
+    end
   end
 end
