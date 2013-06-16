4b81fb3c804c4ac90635999f838b7aed26eaa101
Huy Dinh <dinhhuydh@gmail.com>
2013-06-16 16:20:09 +0700

    when unsigned-in user click on Watch and Buy, the user require to sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  before_filter :set_session
+
+  def after_sign_in_path_for(resource)
+    session[:redirect_to] || root_path
+  end
+
+  private
+
+  def set_session
+    session[:redirect_to] = params[:redirect_to] if params[:redirect_to]
+  end
 end
