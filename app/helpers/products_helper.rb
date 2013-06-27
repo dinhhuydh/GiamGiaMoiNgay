236a4af204a4768f535f9339d9a5a67120e76ef5
Huy Dinh <dinhhuydh@gmail.com>
2013-06-27 23:13:57 +0700

    style the product result after searching

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,7 +1,7 @@
 module ProductsHelper
-  def product_image(product)
+  def product_image(product, style = 'medium')
     if product.images.present?
-      image_tag product.images.first.photo.url
+      image_tag product.images.first.photo.url(style.to_sym)
     else
       image_tag image_path('default_product_image.jpg')
     end
