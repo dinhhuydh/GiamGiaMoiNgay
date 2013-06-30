f75769545e4e4cc768529aab11e09025e0aaea32
Huy Dinh <dinhhuydh@gmail.com>
2013-06-30 19:09:49 +0700

    better factory for Product to make its attributes ' value valide

--- spec/factories/product_factory.rb
+++ spec/factories/product_factory.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :product do
     sequence(:name) {|n| "product #{n}" }
-    price { "#{rand(10)}000000".to_i }
-    sale_by_day { "#{rand(10)}00000".to_i }
+    price { "#{rand(10) + 1}000000".to_i }
+    sale_by_day { "#{rand(10) + 1}00000".to_i }
     public_time { Time.now }
-    initial_price { "#{rand(10)}0000000".to_i }
+    initial_price { "#{rand(10) + 11}000000".to_i }
   end
 end
